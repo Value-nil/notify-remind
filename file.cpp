e734f9f47acab60cbb8c27a0a3004a7444d1cc28
Value_nil <value.nil5@gmail.com>
2023-05-28 20:41:41 +0200

    Implemented settings system Settings can be set on the file, placing the name of the setting in uppercase and using an equal with no spaces(I am searching to a solution for that). Only one setting can be set on one line, since the splitting works only for the first equals. If 2 equals were set on one line, the second equals would enter into the second string.

--- file.cpp
+++ file.cpp
@@ -4,15 +4,34 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <errno.h>
+#include <sys/stat.h>
 
 using namespace std;
 
-void sendNotification(){
-    notify_init("THE app");
+map<string, string> settings = {
+        {"SUMMARY", "Levanta la vista por 20 segundos!"},
+        {"BODY", "Hazlo"},
+        {"APP_NAME", "THE app"},
+        {"EXPIRE_TIME", "-1"},
+        {"NOTIFICATION_URGENCY", "NOTIFY_URGENCY_NORMAL"}
+};
 
-    NotifyNotification* notify = notify_notification_new("Levanta la vista por 20 segundos", "", "");
-    
-    GError** someError;
+
+void sendNotification(){
+    notify_init(settings["APP_NAME"].c_str());
+
+    NotifyNotification* notify = notify_notification_new(settings["SUMMARY"].c_str(), settings["BODY"].c_str(), "");
+    int timeout;
+    try{
+        timeout = stoi(settings["EXPIRE_TIME"]);
+    }
+    catch(invalid_argument){
+        std::cerr << "Invalid number";
+        timeout = -1;
+    }
+    GError** someError = nullptr;
+    notify_notification_set_timeout(notify, timeout);
     gboolean success = notify_notification_show(notify, someError);
     if(!success){
         cerr << (**someError).message;
@@ -21,13 +40,15 @@ void sendNotification(){
 
     notify_uninit();
 }
-vector<string> slice(string toSlice, char slicing){
+vector<string> slice(string toSlice, char slicing, int limit){
     vector<string> toReturn;
     string temp = "";
+    int sliced = 1;
     for(int i = 0; i < toSlice.length(); i++){
-        if(toSlice[i] == slicing){
+        if(toSlice[i] == slicing && sliced != limit){
             toReturn.push_back(temp);
             temp = "";
+            sliced++;
         }
         else{
             temp+=toSlice[i];
@@ -39,26 +60,54 @@ vector<string> slice(string toSlice, char slicing){
     return toReturn;
 }
 
+int getFileSize(){
+    struct stat* fileStat = new struct stat;
+    int success = stat("notifyReminderConfig.dat", fileStat);
+    int size = fileStat->st_size;
+    delete fileStat;
+    if(success == -1){
+        std::cerr << errno;
+        return -1;
+    }
+    
+    else{
+        return size;
+    }
+}
+
+
+
+
+void processFile(char* read){
+    vector<string> result = slice(read, '\n', 0);
+    for(int i = 0; i < result.size(); i++){
+        vector<string> result2 = slice(result[i], '=', 2);
+        if(result2[1] != "" && settings[result2[0]] != ""){
+            settings[result2[0]] = result2[1];
+            std::cout << result2[0] + " is being set to " + result2[1] << '\n';
+        }
+    }
+}
+
 
 
 int main(){
-    int filedes = open("/etc/notifyReminderConfig.dat", O_RDWR, O_CREAT);
-    char buff[32];
-    map<string, string> settings = {
-        {"SUMMARY", "Levanta la vista por 20 segundos!"},
-        {"BODY", "Hazlo"},
-        {"APP_NAME", "THE app"},
-        {"EXPIRE_TIME", "-1"},
-        {"NOTIFICATION_URGENCY", "NOTIFY_URGENCY_NORMAL"}
-    };
-    read(filedes, buff, 32);
-    cout << buff << '\n';
-    vector<string> result = slice(buff, '\n');
-    for(int i = 0; i < result.size(); i++){
-        cout << result[i] << ' ';
+    int filedes = open("notifyReminderConfig.dat", O_RDWR | O_CREAT);
+    if(filedes == -1){
+        std::cerr << errno;
+        return -1;
     }
-    char vuff[] = "";
-    write(filedes, vuff, sizeof(vuff));
+
+    int size = getFileSize();
+    if(size == -1) return -1;
+    char* buff = new char[size+1];
+    read(filedes, buff, size);
+    buff[size] = '\0';
+    cout << buff << '\n';
+    processFile(buff);
+
+    sendNotification();
+    
     close(filedes);
     return 0;
 }
