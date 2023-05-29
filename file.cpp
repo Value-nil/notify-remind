704b4c2e03c5a2c1b2848d9578aa72991f457d07
Value_nil <value.nil5@gmail.com>
2023-05-29 10:14:50 +0200

    Fixed error system and included urgency I misunderstood the error system when calling the notification show function, but now it's fixed. I also included urgency levels as I didn't do it before. Finally, I cleared the code a bit of printing out.

--- file.cpp
+++ file.cpp
@@ -7,6 +7,8 @@
 #include <errno.h>
 #include <sys/stat.h>
 
+#define path "notifyReminderConfig.dat"
+
 using namespace std;
 
 map<string, string> settings = {
@@ -14,29 +16,38 @@ map<string, string> settings = {
         {"BODY", "Hazlo"},
         {"APP_NAME", "THE app"},
         {"EXPIRE_TIME", "-1"},
-        {"NOTIFICATION_URGENCY", "NOTIFY_URGENCY_NORMAL"}
+        {"NOTIFICATION_URGENCY", "1"}
+};
+map<string, NotifyUrgency> urgency = {
+    {"0", NotifyUrgency::NOTIFY_URGENCY_LOW},
+    {"1", NotifyUrgency::NOTIFY_URGENCY_NORMAL},
+    {"2", NotifyUrgency::NOTIFY_URGENCY_CRITICAL}
 };
 
 
 void sendNotification(){
     notify_init(settings["APP_NAME"].c_str());
+    
 
     NotifyNotification* notify = notify_notification_new(settings["SUMMARY"].c_str(), settings["BODY"].c_str(), "");
+    
     int timeout;
     try{
         timeout = stoi(settings["EXPIRE_TIME"]);
     }
     catch(invalid_argument){
-        std::cerr << "Invalid number";
+        cerr << "Invalid number";
         timeout = -1;
     }
-    GError** someError = nullptr;
     notify_notification_set_timeout(notify, timeout);
-    gboolean success = notify_notification_show(notify, someError);
-    if(!success){
-        cerr << (**someError).message;
-    }
 
+    notify_notification_set_urgency(notify, urgency[settings["NOTIFICATION_URGENCY"]]);
+    
+    GError* someError = nullptr;
+    gboolean success = notify_notification_show(notify, &someError);
+    if(!success){
+        cout << "The error is: " << (*someError).message;
+    }
 
     notify_uninit();
 }
@@ -44,6 +55,7 @@ vector<string> slice(string toSlice, char slicing, int limit){
     vector<string> toReturn;
     string temp = "";
     int sliced = 1;
+    
     for(int i = 0; i < toSlice.length(); i++){
         if(toSlice[i] == slicing && sliced != limit){
             toReturn.push_back(temp);
@@ -62,11 +74,13 @@ vector<string> slice(string toSlice, char slicing, int limit){
 
 int getFileSize(){
     struct stat* fileStat = new struct stat;
-    int success = stat("notifyReminderConfig.dat", fileStat);
+    int success = stat(path, fileStat);
     int size = fileStat->st_size;
+
     delete fileStat;
+
     if(success == -1){
-        std::cerr << errno;
+        cerr << "Error when getting file size: " << errno << '\n';
         return -1;
     }
     
@@ -84,7 +98,6 @@ void processFile(char* read){
         vector<string> result2 = slice(result[i], '=', 2);
         if(result2[1] != "" && settings[result2[0]] != ""){
             settings[result2[0]] = result2[1];
-            std::cout << result2[0] + " is being set to " + result2[1] << '\n';
         }
     }
 }
@@ -92,22 +105,26 @@ void processFile(char* read){
 
 
 int main(){
-    int filedes = open("notifyReminderConfig.dat", O_RDWR | O_CREAT);
+    int filedes = open(path, O_RDONLY | O_CREAT);
     if(filedes == -1){
-        std::cerr << errno;
+        cerr << "Error when opening file: " << errno << '\n';
         return -1;
     }
 
     int size = getFileSize();
+    
     if(size == -1) return -1;
     char* buff = new char[size+1];
     read(filedes, buff, size);
     buff[size] = '\0';
-    cout << buff << '\n';
+    
     processFile(buff);
-
+    
     sendNotification();
     
+    
+    
+    delete buff;
     close(filedes);
     return 0;
 }
